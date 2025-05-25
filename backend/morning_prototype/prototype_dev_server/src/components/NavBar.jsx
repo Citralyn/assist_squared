55607ceb7238ad8cb13a4f8e1142db15cdea79f1
Julia Nguyen <juliatn5@uci.edu>
2025-05-25 02:45:31 -0700

    style nav

--- backend/morning_prototype/prototype_dev_server/src/components/NavBar.jsx
+++ backend/morning_prototype/prototype_dev_server/src/components/NavBar.jsx
@@ -1,23 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import styles from '../../styles/NavBar.module.css';
 
 import { Outlet } from "react-router";
 
 export default function NavBar() {
   return (
     <>
-    <Navbar expand='false' className='bg-dark nav_bar'>
-      <Container>
-        <Navbar.Brand className='text-light' href="/">prototype navbar</Navbar.Brand>
-        <Navbar.Toggle className='bg-light' aria-controls="collapser"/>
-        <Navbar.Collapse id="collapser">
-          <Nav style={{float: "right"}}>
-            <Nav.Link className='text-light ' href="/assist">ASSIST</Nav.Link>
-            <Nav.Link className='text-light' href="/peter">PETER</Nav.Link>
+    <Navbar expand='false' fixed="top" variant="dark" className={styles.nav}>
+       <div className={styles.logoContain}>
+        <img className={styles.logo} src="logo.svg" alt="anteater with roman numeral two logo"></img>
+        <Navbar.Brand href="/">ASSIST²</Navbar.Brand>
+        </div>
+        <Navbar.Toggle className={styles.navItem} aria-controls="offcanvasNavbar-expand-${expand}"/>
+        <Navbar.Collapse id="offcanvasNavbar-expand-${expand}">
+          <Nav style={{float: "right", padding: 12, }}>
+            <Nav.Link className='text-light' href="/assist">Assist</Nav.Link>
+            <Nav.Link className='text-light' href="/peter">Resources</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Container>
     </Navbar>
     <Outlet />
     </>
