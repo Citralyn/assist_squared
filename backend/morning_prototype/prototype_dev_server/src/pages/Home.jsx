7b8d60e2cd963af08226d46ba81538b059478c4c
Julia Nguyen <juliatn5@uci.edu>
2025-05-25 03:06:11 -0700

    add global font

--- backend/morning_prototype/prototype_dev_server/src/pages/Home.jsx
+++ backend/morning_prototype/prototype_dev_server/src/pages/Home.jsx
@@ -1,16 +1,26 @@
 import { useNavigate } from 'react-router-dom';
-
+import styles from '../../styles/Home.module.css';
+import "../index.css";
 export default function Home() {
 
 const navigate = useNavigate();
 
   return (
-    <div>
-    <h1>Home</h1>
-          <button onClick={() => navigate('/assist')}>Go to Assist</button>
-      <button onClick={() => navigate('/peter')}>Go to Peter</button>
-
+    <>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
+    <img src="public/anteater.png" alt="anteater clip art" className={styles.anteater}/>
+    <div className={styles.container}>
+        <h1 className={styles.welcomeText}>👋 Hi there,</h1>
+        <h2 className={styles.heroText}>ICS Transfer Student!</h2>
+        <p>Want to prepare for your courses and career? Assess your knowledge and supplement your learning!</p>
     </div>
+    <div className={styles.buttonContainer}>
+      <button onClick={() => navigate('/assist')} className={styles.assistButton}>Assist</button>
+      <button onClick={() => navigate('/peter')} className={styles.peterButton}>Resources</button>
+    </div>
+    </>
   )
 }
 
